4b1e0ed4e855d719604b3ff1f356dbf759928bfb
Lucy Lee <gamesandhannah@gmail.com>
2022-01-14 15:04:15 -0500

    Create folder to store all components

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchField from "./SearchField";
+import GifCard from "./GifCard";
+
+function App() {
+    return (
+        <>
+            <SearchField />
+            <GifCard />
+        </>
+    );
+}
+
+export default App;
---
+++ src/Components/SearchField.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SearchField() {
+    //return (
+    // );
+}
+
+export default SearchField;
+
