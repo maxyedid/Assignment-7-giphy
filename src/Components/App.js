06edbd12c9db9f6035732cc0bda5637b9b006d8c
Lucy Lee <gamesandhannah@gmail.com>
2022-01-16 03:13:54 -0500

    Fix old code to use traditional hooks with Axios
    I will leave the useAxios code commented.
    The problem with my old code was that I'd setGifs to the specific URL
    instead of response.data (the object array), and that's why I was
    struggling to access the URLs in the return. I also fixed the
    dependencies to be set to [setGifs] rather than [gifs] to prevent
    infinite rendering.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import useAxios from 'axios-hooks';
+import axios from 'axios';
 import SearchField from "./SearchField";
 import GifCard from "./GifCard";
 
@@ -7,7 +8,7 @@ const baseURL = "http://api.giphy.com/v1/gifs/"
 const apiKey = "pgpqAc3RpgbuB1cHj0YdXzdl5l3q0vQg"
 
 function App() {
-    /*const [gifs, setGifs] = React.useState(null);
+    const [gifs, setGifs] = React.useState(null);
 
     React.useEffect(async () => {
             try {
@@ -16,40 +17,41 @@ function App() {
                     + apiKey)
                     .then((response) => {
                         console.log(response.data)
-                        console.log(response.data.data[0].url);
-                        setGifs(response.data.data[0].url);
+                        console.log(response.data.data[0].images.original.url);
+                        setGifs(response.data);
                     });
             } catch (error) {
                 console.log(error);
             }
-    },[gifs]);*/
+    },[setGifs]);
 
-    const [{data, loading, error}, refetch] = useAxios(
-        "http://api.giphy.com/v1/gifs/trending?api_key=pgpqAc3RpgbuB1cHj0YdXzdl5l3q0vQg"
-    )
+    // const [{data, loading, error}, refetch] = useAxios(
+    //     "http://api.giphy.com/v1/gifs/trending?api_key=pgpqAc3RpgbuB1cHj0YdXzdl5l3q0vQg"
+    // )
 
-    if (loading) return <p>Loading...</p>
-    if (error) return <p>Error!</p>
+    // if (loading) return <p>Loading...</p>
+    // if (error) return <p>Error!</p>
 
-    // if (!gifs) return null;
+    if (!gifs) return null;
 
     return (
         <>
-        {/*<ul>*/}
-        {/*    <{gifs.data.map(gif =>*/}
-        {/*    <li key={gif.id}>{gif.url}</li>)}>*/}
-        {/*</ul>*/}
+        <ul>
 
-            <div>
-                <button onClick={refetch}>refetch</button>
-                <p>{data.data[0].url}</p>
-                {data.data.map(gif =>
-                <img src={gif.images.original.url}/>)}
-                <img src={data.data[0].images.original.url}/>
+        </ul>
+            {gifs.data.map(gif =>
+            <img key={gif.id} src={gif.images.original.url}/>)}
 
-                {/*<pre>{JSON.stringify(data, null, 2)}</pre>*/}
+            {/*<div>*/}
+            {/*    /!*<button onClick={refetch}>refetch</button>*!/*/}
+            {/*    /!*<p></p>*!/*/}
+            {/*    /!*{data.data.map(gif =>*!/*/}
+            {/*    /!*<img src={gif.images.original.url}/>)}*!/*/}
+            {/*    /!*<img src={data.data[0].images.original.url}/>*!/*/}
 
-            </div>
+            {/*    /!*<pre>{JSON.stringify(data, null, 2)}</pre>*!/*/}
+
+            {/*</div>*/}
         </>
     );
 }
