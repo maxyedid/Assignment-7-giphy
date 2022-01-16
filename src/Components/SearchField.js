6e5e7f66b255e2c06de5162af7a0ebf725c18270
Lucy Lee <gamesandhannah@gmail.com>
2022-01-16 18:26:38 -0500

    Remove console.log lines

--- src/Components/SearchField.js
+++ src/Components/SearchField.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import axios from "axios";
-
-const baseURL = "http://api.giphy.com/v1/gifs/"
-const apiKey = "pgpqAc3RpgbuB1cHj0YdXzdl5l3q0vQg"
 
 const SearchField = ({updateGifs}) => {
     const [search, setSearch] = React.useState("")
     const handleSubmit = (event) => {
         event.preventDefault();
-        // console.log(search);
         updateGifs(search);
     }
 
@@ -17,7 +12,7 @@ const SearchField = ({updateGifs}) => {
             <form onSubmit={handleSubmit}>
                 <input type="text"
                        value={search}
-                       onChange={event => setSearch(event.target.value)}/>
+                       required onChange={event => setSearch(event.target.value)}/>
                 <input type="submit" value="submit"/>
             </form>
         </>
