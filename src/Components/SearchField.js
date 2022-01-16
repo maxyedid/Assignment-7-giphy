384b36a954787c498f174104bbdd66a15ecc79cc
Lucy Lee <gamesandhannah@gmail.com>
2022-01-16 17:20:15 -0500

    Pass search term to App component #2

--- src/Components/SearchField.js
+++ src/Components/SearchField.js
@@ -4,36 +4,12 @@ import axios from "axios";
 const baseURL = "http://api.giphy.com/v1/gifs/"
 const apiKey = "pgpqAc3RpgbuB1cHj0YdXzdl5l3q0vQg"
 
-function SearchField(props) {
+const SearchField = ({updateGifs}) => {
     const [search, setSearch] = React.useState("")
-    const [gifs, setGifs] = React.useState(null)
-
-    async function fetchData() {
-        try {
-            const response = await axios.get(baseURL
-                + "search?q="
-                + {search}
-                + "&api_key="
-                + apiKey)
-                // .then((response) => {
-                //     console.log({search})
-                //     console.log(response.data)
-                //     console.log(response.data.data[0].images.original.url);
-                //     setGifs(response.data);
-                // });
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    React.useEffect(() => {
-        fetchData()
-    },[]);
-
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log({search})
-        fetchData()
+        // console.log(search);
+        updateGifs(search);
     }
 
     return (
@@ -44,10 +20,6 @@ function SearchField(props) {
                        onChange={event => setSearch(event.target.value)}/>
                 <input type="submit" value="submit"/>
             </form>
-
-            {gifs.data.map(gif =>
-                <img key={gif.id} src={gif.images.original.url}/>)}
-
         </>
     );
 }
