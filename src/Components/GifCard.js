5c1f8f15208ed2430b8c7102f127df40df964e13
Lucy Lee <gamesandhannah@gmail.com>
2022-01-17 18:35:22 -0500

    Clean up whitespace and add event listener for click

--- src/Components/GifCard.js
+++ src/Components/GifCard.js
@@ -1,47 +1,32 @@
 import React from 'react';
 
-function GifCard(props) {
-    // Sort by newest to oldest import date time
-    // Sort by oldest to newest import date time
-    const sortDate = order =>{
-        switch(order) {
-            case 'old': return this.setState({data : this.state.data.sort((a,b)=> a-b) })
-            case 'new' : return this.setState({data : this.state.data.sort((a,b) => b-a)})
-       }
-    }
-
-    const sortSize = order => {
-        switch(order) {
-        case 'small': return this.setState({data : this.state.data.sort((a,b)=> a-b) });
-        case 'large' : return this.setState({data : this.state.data.sort((a,b) => b-a)});
-        }
+function GifCard({sortGifs}) {
+    const [sort, setSort] = React.useState("")
+    const handleClick = (event) => {
+        sortGifs(sort);
     }
 
     return (
         <>
-            <input type="radio"
-                   id="parameter1"
-                   name="parameters"
+            <input type="radio" id="parameter1" name="parameters"
                    value="new"
-                   onClick={sortDate}/>
+                   required onChange={event => setSort(event.target.value)}
+                   onClick={handleClick}/>
             <label htmlFor="parameter1">Sort by newest</label>
-            <input type="radio"
-                   id="parameter2"
-                   name="parameters"
+            <input type="radio" id="parameter2" name="parameters"
                    value="old"
-                   onClick={sortDate}/>
+                   required onChange={event => setSort(event.target.value)}
+                   onClick={handleClick}/>
             <label htmlFor="parameter2">Sort by oldest</label>
-            <input type="radio"
-                   id="parameter3"
-                   name="parameters"
+            <input type="radio" id="parameter3" name="parameters"
                    value="small"
-                   onClick={sortSize}/>
+                   required onChange={event => setSort(event.target.value)}
+                   onClick={handleClick}/>
             <label htmlFor="parameter1">Sort by smallest</label>
-            <input type="radio"
-                   id="parameter3"
-                   name="parameters"
+            <input type="radio" id="parameter4" name="parameters"
                    value="large"
-                   onClick={sortSize}/>
+                   required onChange={event => setSort(event.target.value)}
+                   onClick={handleClick}/>
             <label htmlFor="parameter1">Sort by largest</label>
         </>
     )
