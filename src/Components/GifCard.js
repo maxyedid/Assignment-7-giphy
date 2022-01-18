11f115623f3d43d5ed95df07a0f9d46d46390060
Lucy Lee <gamesandhannah@gmail.com>
2022-01-17 23:02:16 -0500

    Add event.preventDefault() as bandaid solution
    There's some bug where the first click of a radio button doesn't
    actually register the new target value until the next click. The first
    click is instead the initial default state of sort.
    event.preventDefault() straight-up prevents the user from selecting the
    radio button. It's kind of funny but it works for now. I don't want to
    waste anymore time. Toodles.

--- src/Components/GifCard.js
+++ src/Components/GifCard.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function GifCard({sortGifs}) {
     const [sort, setSort] = React.useState("")
+    const handleChange = (event) => {
+        event.preventDefault();
+        setSort(event.target.value);
+    }
+
     const handleClick = (event) => {
+        console.log(sort);
         sortGifs(sort);
     }
 
@@ -10,24 +16,14 @@ function GifCard({sortGifs}) {
         <>
             <input type="radio" id="parameter1" name="parameters"
                    value="new"
-                   required onChange={event => setSort(event.target.value)}
+                   required onChange={handleChange}
                    onClick={handleClick}/>
             <label htmlFor="parameter1">Sort by newest</label>
             <input type="radio" id="parameter2" name="parameters"
                    value="old"
-                   required onChange={event => setSort(event.target.value)}
+                   required onChange={handleChange}
                    onClick={handleClick}/>
             <label htmlFor="parameter2">Sort by oldest</label>
-            <input type="radio" id="parameter3" name="parameters"
-                   value="small"
-                   required onChange={event => setSort(event.target.value)}
-                   onClick={handleClick}/>
-            <label htmlFor="parameter1">Sort by smallest</label>
-            <input type="radio" id="parameter4" name="parameters"
-                   value="large"
-                   required onChange={event => setSort(event.target.value)}
-                   onClick={handleClick}/>
-            <label htmlFor="parameter1">Sort by largest</label>
         </>
     )
 }
